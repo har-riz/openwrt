d7909f39528df40fa031b48be0907dd46bf2bd18
har-riz <56026313+har-riz@users.noreply.github.com>
2021-02-02 14:21:18 +0700

    update ipv4 only

--- package/lean/luci-app-sfe/luasrc/model/cbi/sfe.lua
+++ package/lean/luci-app-sfe/luasrc/model/cbi/sfe.lua
@@ -21,11 +21,13 @@ wifi.rmempty = false
 wifi.description = translate("Enable Bridge Acceleration (may be functional conflict with bridge-mode VPN Server)")
 wifi:depends("enabled", 1)
 
+if nixio.fs.access("/proc/sys/net/ipv6") then
 ipv6 = s:option(Flag, "ipv6", translate("IPv6 Acceleration"))
 ipv6.default = 0
 ipv6.rmempty = false
 ipv6.description = translate("Enable IPv6 Acceleration")
 ipv6:depends("enabled", 1)
+end
 
 fullcone = s:option(Flag, "fullcone", translate("Enable FullCone-NAT"))
 fullcone.default = 0
